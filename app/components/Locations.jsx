40464b0f1f735a9aefc3f1df70669fed672bc900
Apple <apple@Apples-MacBook-Air.local>
2026-05-13 18:27:34 +0530

    Update M3M 65th Avenue location hours
    - Change hours from 10:15 AM – 10:30 PM to 11:00 AM – 09:00 PM

--- app/components/Locations.jsx
+++ app/components/Locations.jsx
@@ -19,7 +19,7 @@ const locations = [
     name: 'M3M 65th Avenue',
     address: 'Shop R7, UG 02, Sector 65',
     phone: '+91 98111 12254',
-    hours: '10:15 AM – 10:30 PM (Daily)',
+    hours: '11:00 AM – 09:00 PM (Daily)',
     rating: 4.6,
     mapEmbed: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3507.5432109876!2d77.3456!3d28.3456!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMjjCsDIwJzU0LjMiTiA3N8KwMjAnNDUuNiJF!5e0!3m2!1sen!2sin!4v0987654321',
   },
